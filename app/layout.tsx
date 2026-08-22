83f3c2a31c7aa0a25df4b4e70850dabf8f5f7bc7
Ayo Richard ABE [gODtECH] <ayoabe24@gmail.com>
2026-08-22 12:48:45 +0100

    fix: load responsive UI hardening layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./responsive-ui.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import AmbientPiano from "@/components/AmbientPiano";
